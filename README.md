4a3d2f7aa1f839370dd481c3216ca2b99d0e859b
David Beveridge <davidjbeveridge@gmail.com>
2012-05-24 15:31:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ReadMe
+
+
+## Deployment
+
+```
+ey deploy
+```
+Remove
+## Thanks
+
+The original scaffold for this application was created by [App Scrolls](http://appscrolls.org).
+
+The project was created with the following scrolls:
+
+* rails_basics
+* twitter_bootstrap
+* simple_form
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frugal::Application.config.secret_token = 'e20e33376f4c3209374dd289af44821ce1ca8f2ce3bd0d28ee7db1793c8be756a655d53e3d23098c5915d66ad7183b4c0f8e1cc2508474589501946f977de4e4'
